e7ba0809c94f32077501d30a059a622de65c10ae
zhaosiying12138 <neucszhaopeng2016@163.com>
2023-07-14 00:45:14 -0700

    spacepar: add codengen function using Quillere Algo implemented manually
    Signed-off-by: zhaosiying12138 <neucszhaopeng2016@163.com>

--- isl_spacepar.c
+++ isl_spacepar.c
@@ -32,6 +32,46 @@
 #include <isl_ast_build_expr.h>
 #include <isl/options.h>
 
+static void zsy_codegen(__isl_keep isl_union_map *schedule)
+{
+	isl_ctx *ctx;
+	isl_ast_build *build;
+	isl_ast_node *tree;
+	
+	ctx = isl_union_map_get_ctx(schedule);
+	build = isl_ast_build_from_context(isl_set_read_from_str(ctx, "{ : }"));
+	tree = isl_ast_build_node_from_schedule_map(build, isl_union_map_copy(schedule));
+	printf("%s\n", isl_ast_node_to_C_str(tree));
+	isl_ast_node_free(tree);
+}
+
+static __isl_give isl_basic_map *zsy_isl_basic_map_set_range_dim_constant_val(__isl_take isl_basic_map *bmap, unsigned int pos, int val)
+{
+	isl_constraint *c;
+	bmap = isl_basic_map_project_out(bmap, isl_dim_out, pos, 1);
+	bmap = isl_basic_map_insert_dims(bmap, isl_dim_out, pos, 1);
+	c = isl_constraint_alloc_equality(isl_local_space_from_space(isl_basic_map_get_space(bmap)));
+	c = isl_constraint_set_coefficient_si(c, isl_dim_out, pos, -1);
+	c = isl_constraint_set_constant_si(c, val);
+	bmap = isl_basic_map_add_constraint(bmap, c);
+	return bmap;
+}
+
+static __isl_give isl_union_map *zsy_isl_union_map_set_range_dim_constant_val(__isl_take isl_union_map *umap, unsigned int pos, int val)
+{
+	isl_union_map *res_umap = isl_union_map_empty(isl_union_map_get_space(umap));
+	isl_map_list *map_list = isl_union_map_get_map_list(umap);
+	for (int i = 0; i < isl_map_list_n_map(map_list); i++) {
+		isl_map *map = isl_map_list_get_map(map_list, i);
+		isl_basic_map_list *bmap_list = isl_map_get_basic_map_list(map);
+		assert(isl_basic_map_list_n_basic_map(bmap_list) == 1);
+		isl_basic_map *bmap = isl_basic_map_list_get_basic_map(bmap_list, 0);
+		bmap = zsy_isl_basic_map_set_range_dim_constant_val(bmap, pos, val);
+		res_umap = isl_union_map_add_map(res_umap, isl_map_from_basic_map(bmap));
+	}
+	return res_umap;
+}
+
 int zsy_spacepar_compute_schedule(isl_ctx *ctx)
 {
 	const char *d, *w, *r, *s, *con;
@@ -51,7 +91,7 @@ int zsy_spacepar_compute_schedule(isl_ctx *ctx)
 	r = "{ S1[i, j] -> X[i, j]; S1[i, j] -> Y[i - 1, j]; S2[i, j] -> Y[i, j]; S2[i, j] -> X[i, j - 1] }";
 	s = "{ S1[i, j] -> [0, i, 0, j, 0]; S2[i, j] -> [0, i, 0, j, 1] }";
 
-	printf("\nCompute schedule for Space-Partition-Constraints Demo:\n");
+	printf("\nCompute schedule:\n");
 	CON = isl_set_read_from_str(ctx, con);
 	D = isl_union_set_read_from_str(ctx, d);
 	W = isl_union_map_read_from_str(ctx, w);
@@ -108,7 +148,7 @@ int zsy_spacepar_isl_codegen(isl_ctx *ctx)
 	d = "{ S1[i, j] : 1 <= i <= 100 and 1 <= j <= 100; S2[i, j] : 1 <= i <= 100 and 1 <= j <= 100 }";
 	s = "{ S1[i, j] -> [i - j - 1, 0, i, 0, j, 0]; S2[i, j] -> [i - j, 0, i, 0, j, 1] }";
 
-	printf("Codegen with ISL-native-ast-build:\n");
+	printf("\nCodegen with ISL-native-ast-build:\n");
 	CON = isl_set_read_from_str(ctx, con);
 	D = isl_union_set_read_from_str(ctx, d);
 	S = isl_union_map_read_from_str(ctx, s);
@@ -123,6 +163,218 @@ int zsy_spacepar_isl_codegen(isl_ctx *ctx)
 
 	return 0;
 }
+
+int zsy_test_space_partition_Quillere_step1(isl_ctx *ctx)
+{
+	const char *domain_str[2], *domain_name_str[2], *trans_str[2], *schedule_orig_str;
+	isl_set *domain[2];
+	isl_map *trans[2], *tmp_map;
+	isl_union_map *schedule_orig;
+	isl_set *domain_trans[2], *domain_trans_1dim[2], *domain_trans_2dim[2];
+	isl_set *domain_trans_1dim_split[2][2], *domain_trans_2dim_split[2][2];
+	isl_union_set *tmp_split_domain[3];
+	isl_union_map *tmp_split_sched[3];
+	isl_union_map *total_sched;
+
+	printf("\nCodegen using Quillere Algo implemented manually:\n");
+	printf("[Step 1]:\n");
+	domain_str[0] = "{ S1[i, j] : 1 <= i <= 100 and 1 <= j <= 100 }";
+	domain_str[1] = "{ S2[i, j] : 1 <= i <= 100 and 1 <= j <= 100 }";
+	trans_str[0] = "{ S1[i, j] -> [p = i - j - 1, i, j] }";
+	trans_str[1] = "{ S2[i, j] -> [p = i - j    , i, j] }";
+	schedule_orig_str = "{ S1[p, i, j] -> [0, p, 0, i, 0, j, 0]; S2[p, i, j] -> [0, p, 0, i, 0, j, 1] }";
+
+	domain[0] = isl_set_read_from_str(ctx, domain_str[0]);
+	domain[1] = isl_set_read_from_str(ctx, domain_str[1]);
+	domain_name_str[0] = isl_set_get_tuple_name(domain[0]);
+	domain_name_str[1] = isl_set_get_tuple_name(domain[1]);
+	trans[0] = isl_map_read_from_str(ctx, trans_str[0]);
+	trans[1] = isl_map_read_from_str(ctx, trans_str[1]);
+	schedule_orig = isl_union_map_read_from_str(ctx, schedule_orig_str);
+
+	trans[0] = isl_map_set_tuple_name(trans[0], isl_dim_out, domain_name_str[0]);
+	trans[1] = isl_map_set_tuple_name(trans[1], isl_dim_out, domain_name_str[1]);
+	domain_trans[0] = isl_set_apply(domain[0], trans[0]);
+	domain_trans[1] = isl_set_apply(domain[1], trans[1]);
+
+	domain_trans_1dim[0] = isl_set_eliminate(isl_set_copy(domain_trans[0]), isl_dim_set, 1, 2);
+	domain_trans_1dim[1] = isl_set_eliminate(isl_set_copy(domain_trans[1]), isl_dim_set, 1, 2);
+	domain_trans_1dim[0] = isl_set_set_tuple_name(domain_trans_1dim[0], "");
+	domain_trans_1dim[1] = isl_set_set_tuple_name(domain_trans_1dim[1], "");
+
+	domain_trans_1dim_split[0][0] = isl_set_subtract(isl_set_copy(domain_trans_1dim[0]), isl_set_copy(domain_trans_1dim[1]));
+	domain_trans_1dim_split[0][1] = isl_set_intersect(isl_set_copy(domain_trans_1dim[0]), isl_set_copy(domain_trans_1dim[1]));
+	domain_trans_1dim_split[1][0] = isl_set_intersect(isl_set_copy(domain_trans_1dim[1]), isl_set_copy(domain_trans_1dim[0]));
+	domain_trans_1dim_split[1][1] = isl_set_subtract(isl_set_copy(domain_trans_1dim[1]), isl_set_copy(domain_trans_1dim[0]));
+
+	tmp_map = isl_set_lex_gt_set(isl_set_copy(domain_trans_1dim_split[0][0]), isl_set_copy(domain_trans_1dim_split[0][1]));
+	domain_trans_1dim_split[0][0] = isl_set_set_tuple_name(domain_trans_1dim_split[0][0], domain_name_str[0]);
+	domain_trans_1dim_split[0][1] = isl_set_set_tuple_name(domain_trans_1dim_split[0][1], domain_name_str[0]);
+	domain_trans_1dim_split[1][0] = isl_set_set_tuple_name(domain_trans_1dim_split[1][0], domain_name_str[1]);
+	domain_trans_1dim_split[1][1] = isl_set_set_tuple_name(domain_trans_1dim_split[1][1], domain_name_str[1]);
+	domain_trans_1dim_split[0][0] = isl_set_intersect(domain_trans_1dim_split[0][0], isl_set_copy(domain_trans[0]));
+	domain_trans_1dim_split[0][1] = isl_set_intersect(domain_trans_1dim_split[0][1], isl_set_copy(domain_trans[0]));
+	domain_trans_1dim_split[1][0] = isl_set_intersect(domain_trans_1dim_split[1][0], isl_set_copy(domain_trans[1]));
+	domain_trans_1dim_split[1][1] = isl_set_intersect(domain_trans_1dim_split[1][1], isl_set_copy(domain_trans[1]));
+
+	if (isl_map_is_empty(tmp_map)) {
+		tmp_split_domain[0] = isl_union_set_from_set(domain_trans_1dim_split[0][0]);
+		tmp_split_domain[1] = isl_union_set_union(isl_union_set_from_set(domain_trans_1dim_split[0][1]),
+													isl_union_set_from_set(domain_trans_1dim_split[1][0]));
+		tmp_split_domain[2] = isl_union_set_from_set(domain_trans_1dim_split[1][1]);
+	} else {
+		tmp_split_domain[2] = isl_union_set_from_set(domain_trans_1dim_split[0][0]);
+		tmp_split_domain[1] = isl_union_set_union(isl_union_set_from_set(domain_trans_1dim_split[0][1]),
+													isl_union_set_from_set(domain_trans_1dim_split[1][0]));
+		tmp_split_domain[0] = isl_union_set_from_set(domain_trans_1dim_split[1][1]);
+	}
+
+	isl_map_free(tmp_map);
+	tmp_split_sched[0] = zsy_isl_union_map_set_range_dim_constant_val(isl_union_map_copy(schedule_orig), 0, 0);
+	tmp_split_sched[1] = zsy_isl_union_map_set_range_dim_constant_val(isl_union_map_copy(schedule_orig), 0, 1);
+	tmp_split_sched[2] = zsy_isl_union_map_set_range_dim_constant_val(isl_union_map_copy(schedule_orig), 0, 2);
+	tmp_split_sched[0] = isl_union_map_intersect_domain(tmp_split_sched[0], isl_union_set_copy(tmp_split_domain[0]));
+	tmp_split_sched[1] = isl_union_map_intersect_domain(tmp_split_sched[1], isl_union_set_copy(tmp_split_domain[1]));
+	tmp_split_sched[2] = isl_union_map_intersect_domain(tmp_split_sched[2], isl_union_set_copy(tmp_split_domain[2]));
+
+
+	zsy_codegen(tmp_split_sched[0]);
+	zsy_codegen(tmp_split_sched[1]);
+	zsy_codegen(tmp_split_sched[2]);
+
+	return 0;
+}
+
+int zsy_test_space_partition_Quillere_step2(isl_ctx *ctx)
+{
+	const char *domain_str[2], *domain_name_str[2], *trans_str[2], *schedule_orig_str;
+	isl_set *domain[2];
+	isl_map *trans[2], *tmp_map;
+	isl_union_map *schedule_orig;
+	isl_set *domain_trans[2], *domain_trans_1dim[2], *domain_trans_2dim[2];
+	isl_set *domain_trans_1dim_split[2][2], *domain_trans_2dim_split[2][2];
+	isl_union_set *tmp_split_domain[3];
+	isl_union_map *tmp_split_sched[3];
+	isl_union_map *total_sched;
+
+	printf("[Step 2]:\n");
+	domain_str[0] = "{ S1[i, j] : 1 <= i <= 100 and 1 <= j <= 100 }";
+	domain_str[1] = "{ S2[i, j] : 1 <= i <= 100 and 1 <= j <= 100 }";
+	trans_str[0] = "{ S1[i, j] -> [p = i - j - 1, i, j] }";
+	trans_str[1] = "{ S2[i, j] -> [p = i - j    , i, j] }";
+	schedule_orig_str = "{ S1[p, i, j] -> [0, p, 0, i, 0, j, 0]; S2[p, i, j] -> [0, p, 0, i, 0, j, 1] }";
+
+	domain[0] = isl_set_read_from_str(ctx, domain_str[0]);
+	domain[1] = isl_set_read_from_str(ctx, domain_str[1]);
+	domain_name_str[0] = isl_set_get_tuple_name(domain[0]);
+	domain_name_str[1] = isl_set_get_tuple_name(domain[1]);
+	trans[0] = isl_map_read_from_str(ctx, trans_str[0]);
+	trans[1] = isl_map_read_from_str(ctx, trans_str[1]);
+	schedule_orig = isl_union_map_read_from_str(ctx, schedule_orig_str);
+
+	trans[0] = isl_map_set_tuple_name(trans[0], isl_dim_out, domain_name_str[0]);
+	trans[1] = isl_map_set_tuple_name(trans[1], isl_dim_out, domain_name_str[1]);
+	domain_trans[0] = isl_set_apply(domain[0], trans[0]);
+	domain_trans[1] = isl_set_apply(domain[1], trans[1]);
+
+	domain_trans_1dim[0] = isl_set_eliminate(isl_set_copy(domain_trans[0]), isl_dim_set, 1, 2);
+	domain_trans_1dim[1] = isl_set_eliminate(isl_set_copy(domain_trans[1]), isl_dim_set, 1, 2);
+	domain_trans_1dim[0] = isl_set_set_tuple_name(domain_trans_1dim[0], "");
+	domain_trans_1dim[1] = isl_set_set_tuple_name(domain_trans_1dim[1], "");
+
+	domain_trans_1dim_split[0][0] = isl_set_subtract(isl_set_copy(domain_trans_1dim[0]), isl_set_copy(domain_trans_1dim[1]));
+	domain_trans_1dim_split[0][1] = isl_set_intersect(isl_set_copy(domain_trans_1dim[0]), isl_set_copy(domain_trans_1dim[1]));
+	domain_trans_1dim_split[1][0] = isl_set_intersect(isl_set_copy(domain_trans_1dim[1]), isl_set_copy(domain_trans_1dim[0]));
+	domain_trans_1dim_split[1][1] = isl_set_subtract(isl_set_copy(domain_trans_1dim[1]), isl_set_copy(domain_trans_1dim[0]));
+
+	tmp_map = isl_set_lex_gt_set(isl_set_copy(domain_trans_1dim_split[0][0]), isl_set_copy(domain_trans_1dim_split[0][1]));
+	domain_trans_1dim_split[0][0] = isl_set_set_tuple_name(domain_trans_1dim_split[0][0], domain_name_str[0]);
+	domain_trans_1dim_split[0][1] = isl_set_set_tuple_name(domain_trans_1dim_split[0][1], domain_name_str[0]);
+	domain_trans_1dim_split[1][0] = isl_set_set_tuple_name(domain_trans_1dim_split[1][0], domain_name_str[1]);
+	domain_trans_1dim_split[1][1] = isl_set_set_tuple_name(domain_trans_1dim_split[1][1], domain_name_str[1]);
+	domain_trans_1dim_split[0][0] = isl_set_intersect(domain_trans_1dim_split[0][0], isl_set_copy(domain_trans[0]));
+	domain_trans_1dim_split[0][1] = isl_set_intersect(domain_trans_1dim_split[0][1], isl_set_copy(domain_trans[0]));
+	domain_trans_1dim_split[1][0] = isl_set_intersect(domain_trans_1dim_split[1][0], isl_set_copy(domain_trans[1]));
+	domain_trans_1dim_split[1][1] = isl_set_intersect(domain_trans_1dim_split[1][1], isl_set_copy(domain_trans[1]));
+
+	if (isl_map_is_empty(tmp_map)) {
+		tmp_split_domain[0] = isl_union_set_from_set(domain_trans_1dim_split[0][0]);
+		tmp_split_domain[2] = isl_union_set_from_set(domain_trans_1dim_split[1][1]);
+	} else {
+		tmp_split_domain[2] = isl_union_set_from_set(domain_trans_1dim_split[0][0]);
+		tmp_split_domain[0] = isl_union_set_from_set(domain_trans_1dim_split[1][1]);
+	}
+
+	isl_map_free(tmp_map);
+	tmp_split_sched[0] = zsy_isl_union_map_set_range_dim_constant_val(isl_union_map_copy(schedule_orig), 0, 0);
+	tmp_split_sched[2] = zsy_isl_union_map_set_range_dim_constant_val(isl_union_map_copy(schedule_orig), 0, 2);
+	tmp_split_sched[0] = isl_union_map_intersect_domain(tmp_split_sched[0], isl_union_set_copy(tmp_split_domain[0]));
+	tmp_split_sched[2] = isl_union_map_intersect_domain(tmp_split_sched[2], isl_union_set_copy(tmp_split_domain[2]));
+	//total_sched = isl_union_map_union(tmp_split_sched[0], tmp_split_sched[2]);
+	zsy_codegen(tmp_split_sched[0]);
+
+	{
+		isl_union_map *tmp_split_sched[3];
+
+		domain_trans_2dim[0] = isl_set_eliminate(isl_set_copy(domain_trans_1dim_split[0][1]), isl_dim_set, 2, 1);
+		domain_trans_2dim[1] = isl_set_eliminate(isl_set_copy(domain_trans_1dim_split[1][0]), isl_dim_set, 2, 1);
+		domain_trans_2dim[0] = isl_set_set_tuple_name(domain_trans_2dim[0], "");
+		domain_trans_2dim[1] = isl_set_set_tuple_name(domain_trans_2dim[1], "");
+		domain_trans_2dim_split[0][0] = isl_set_subtract(isl_set_copy(domain_trans_2dim[0]), isl_set_copy(domain_trans_2dim[1]));
+		domain_trans_2dim_split[0][1] = isl_set_intersect(isl_set_copy(domain_trans_2dim[0]), isl_set_copy(domain_trans_2dim[1]));
+		domain_trans_2dim_split[1][0] = isl_set_intersect(isl_set_copy(domain_trans_2dim[1]), isl_set_copy(domain_trans_2dim[0]));
+		domain_trans_2dim_split[1][1] = isl_set_subtract(isl_set_copy(domain_trans_2dim[1]), isl_set_copy(domain_trans_2dim[0]));
+
+		tmp_map = isl_set_lex_gt_set(isl_set_copy(domain_trans_2dim_split[0][0]), isl_set_copy(domain_trans_2dim_split[0][1]));
+		// add constraints for { [p, i1, i2] -> [p', o1, o2] : p = p' }
+		isl_constraint *c;
+		c = isl_constraint_alloc_equality(isl_local_space_from_space(isl_map_get_space(tmp_map)));
+		c = isl_constraint_set_coefficient_si(c, isl_dim_in, 0, 1);
+		c = isl_constraint_set_coefficient_si(c, isl_dim_out, 0, -1);
+		c = isl_constraint_set_constant_si(c, 0);
+		tmp_map = isl_map_add_constraint(tmp_map, c);
+		domain_trans_2dim_split[0][0] = isl_set_set_tuple_name(domain_trans_2dim_split[0][0], domain_name_str[0]);
+		domain_trans_2dim_split[0][1] = isl_set_set_tuple_name(domain_trans_2dim_split[0][1], domain_name_str[0]);
+		domain_trans_2dim_split[1][0] = isl_set_set_tuple_name(domain_trans_2dim_split[1][0], domain_name_str[1]);
+		domain_trans_2dim_split[1][1] = isl_set_set_tuple_name(domain_trans_2dim_split[1][1], domain_name_str[1]);
+		domain_trans_2dim_split[0][0] = isl_set_intersect(domain_trans_2dim_split[0][0], isl_set_copy(domain_trans[0]));
+		domain_trans_2dim_split[0][1] = isl_set_intersect(domain_trans_2dim_split[0][1], isl_set_copy(domain_trans[0]));
+		domain_trans_2dim_split[1][0] = isl_set_intersect(domain_trans_2dim_split[1][0], isl_set_copy(domain_trans[1]));
+		domain_trans_2dim_split[1][1] = isl_set_intersect(domain_trans_2dim_split[1][1], isl_set_copy(domain_trans[1]));
+
+		if (isl_map_is_empty(tmp_map)) {
+			tmp_split_domain[2] = isl_union_set_from_set(domain_trans_2dim_split[1][1]);
+			tmp_split_domain[1] = isl_union_set_union(isl_union_set_from_set(domain_trans_2dim_split[0][1]),
+												isl_union_set_from_set(domain_trans_2dim_split[1][0]));
+			tmp_split_domain[0] = isl_union_set_from_set(domain_trans_2dim_split[0][0]);
+		} else {
+			tmp_split_domain[0] = isl_union_set_from_set(domain_trans_2dim_split[1][1]);
+			tmp_split_domain[1] = isl_union_set_union(isl_union_set_from_set(domain_trans_2dim_split[0][1]),
+												isl_union_set_from_set(domain_trans_2dim_split[1][0]));
+			tmp_split_domain[2] = isl_union_set_from_set(domain_trans_2dim_split[0][0]);
+		}
+
+		isl_map_free(tmp_map);
+		tmp_split_sched[0] = zsy_isl_union_map_set_range_dim_constant_val(isl_union_map_copy(schedule_orig), 0, 1);
+		tmp_split_sched[0] = zsy_isl_union_map_set_range_dim_constant_val(tmp_split_sched[0], 2, 0);
+		tmp_split_sched[1] = zsy_isl_union_map_set_range_dim_constant_val(isl_union_map_copy(schedule_orig), 0, 1);
+		tmp_split_sched[1] = zsy_isl_union_map_set_range_dim_constant_val(tmp_split_sched[1], 2, 1);
+		tmp_split_sched[2] = zsy_isl_union_map_set_range_dim_constant_val(isl_union_map_copy(schedule_orig), 0, 1);
+		tmp_split_sched[2] = zsy_isl_union_map_set_range_dim_constant_val(tmp_split_sched[2], 2, 2);
+
+		tmp_split_sched[0] = isl_union_map_intersect_domain(tmp_split_sched[0], isl_union_set_copy(tmp_split_domain[0]));
+		tmp_split_sched[1] = isl_union_map_intersect_domain(tmp_split_sched[1], isl_union_set_copy(tmp_split_domain[1]));
+		tmp_split_sched[2] = isl_union_map_intersect_domain(tmp_split_sched[2], isl_union_set_copy(tmp_split_domain[2]));
+
+		total_sched = isl_union_map_union(tmp_split_sched[0], tmp_split_sched[1]);
+		total_sched = isl_union_map_union(total_sched, tmp_split_sched[2]);
+		zsy_codegen(total_sched);
+	}
+	zsy_codegen(tmp_split_sched[2]);
+
+	return 0;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -139,6 +391,8 @@ int main(int argc, char **argv)
 				" Technology, LiuYueCity Academy of Science\n");
 	zsy_spacepar_compute_schedule(ctx);
 	zsy_spacepar_isl_codegen(ctx);
+	zsy_test_space_partition_Quillere_step1(ctx);
+	zsy_test_space_partition_Quillere_step2(ctx);
 	isl_ctx_free(ctx);
 	return 0;
 error:
